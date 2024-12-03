787a9d76a64f9633b650c294c633a49794a56e28
MaananeZakaria <zakaria.maanane@laplateforme.io>
2024-12-03 14:37:29 +0100

    Add files via upload

--- Portfolio_pyth_.py
+++ Portfolio_pyth_.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Route principale
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('Portfolio_.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
